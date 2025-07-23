e31ab2227f3484280f308e7d4956684ba49b3a0e
Davinn354 <daviipad53@gmail.com>
2025-07-23 10:08:59 +0700

    first commit

---
+++ src/data/aboutData.jsx
@@ -0,0 +1,36 @@
+const aboutData = {
+    title: "About Me",
+    subtitle: "Discover my journey, passions, and the story behind my work",
+    image: "/assets/profile.jpg",
+
+    biodata: [
+        { label: "Name", value: "Muhammad Davin Surya", icon: "bx bx-id-card" },
+        { label: "Date of Birth", value: "April,12-2005", icon: "bx bx-calendar" },
+        { label: "Place of Birth", value: "Jakarta, Indonesia", icon: "bx bx-map" },
+        { label: "Email", value: "davinipad53l@gmail.com", icon: "bx bx-envelope" },
+        { label: "Phone", value: "+62 812 3095 3589", icon: "bx bx-phone" },
+        { label: "Education", value: "Universitas Muhammadiyah Yogyakarta", icon: "bx bx-book" },
+        { label: "GPA", value: "3.55 / 4.00", icon: "bx bx-award" },
+    ],
+
+    resume: {
+        label: "Download My Resume",
+        href: null,
+        icon: "bx bx-download",
+    },
+
+    aboutNarrative: {
+        whoAmI: {
+            text: `I'm a frontend developer who crafts responsive, interactive, and clean UIs. With a background in Informatics Engineering, I blend design sense with technical logic.`,
+            icon: "bx-info-circle"
+        },
+        approach: {
+            text: `I focus on user-centered design, performance, and accessibility   always evolving with modern tech to deliver seamless digital experiences.`,
+            icon: "bx-bulb"
+        }
+    }
+
+
+};
+
+export default aboutData;
